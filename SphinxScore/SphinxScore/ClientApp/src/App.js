984c159694ad5dcd3d7607ed0b127436bc224766
sohad-hossam <sohad95husam@gmail.com>
2023-12-22 19:38:48 +0200

    adjusting front-end config

--- SphinxScore/SphinxScore/ClientApp/src/App.js
+++ SphinxScore/SphinxScore/ClientApp/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './custom.css';
 import Home from "./components/Home";
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+    console.log("heree")
     return (
         <BrowserRouter>
             <Route index component={Home} />
-            <Route path='/counter' component={Counter} />
-            <Route path='/fetch-data' component={FetchData} />
         </BrowserRouter>
     );
 }
--- SphinxScore/SphinxScore/ClientApp/src/components/Home.js
+++ SphinxScore/SphinxScore/ClientApp/src/components/Home.js
@@ -1,39 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
 function Home() {
-   React.useEffect(() => {
+    React.useEffect(() => {
         const fetchData = async () => {
             try {
-                // GET request
                 const getResponse = await axios.get("https://localhost:44345/api/user/users");
                 console.log(getResponse.data);
-
-                // POST request
-               //const postResponse = await axios.post("https://localhost:44345/api/user/AddUser");
-               // console.log(postResponse.data);
             } catch (error) {
                 console.error(error);
             }
         };
-
         fetchData();
     }, []);
+    // Syntax: useState(initialValue)
+    const [count, setCount] = useState(0); // Initializing state variable 'count' with initial value 0
+
+    const increment = () => {
+        setCount(count + 1); // Updating 'count' using the setCount function
+    };
+
+    const decrement = () => {
+        setCount(count - 1); // Updating 'count' using the setCount function
+    };
     return (
         <div>
-            <h1>Hello, world!</h1>
-            <p>Welcome to your new single-page application, built with:</p>
-            <ul>
-                <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-                <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-                <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-            </ul>
-            <p>To help you get started, we have also set up:</p>
-            <ul>
-                <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-                <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-                <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-            </ul>
-            <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     );
 }
